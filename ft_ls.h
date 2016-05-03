c1cb2621303cb4fe7de50d2e19696d50f50b0747
Dylan BOURDON <dbourdon@student.42.fr>
2016-05-03 15:13:33 +0200

    structure dans le header

--- ft_ls.h
+++ ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: dbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/29 15:44:08 by dbourdon          #+#    #+#             */
-/*   Updated: 2016/04/30 15:28:01 by dbourdon         ###   ########.fr       */
+/*   Updated: 2016/05/03 14:53:25 by dbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,32 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include "libft/libft.h"
+# include <dirent.h>
+# include <sys/types.h>
+# include <sys/stat.h>
 
+typedef struct				s_simple_listing
+{
+	char					*nom;
+	int						type;
+	struct s_simple_listing	*next;
+}							t_sliste;
 
-void	ft_erreur(char *str, int mode);
-int		*ft_detection_option(char *str, int *option);
-int		*ft_lecture_option(char ***argv);
+typedef struct				s_compl_listing
+{
+	char					*nom;
+	int						type;
+	char					*droits;
+	int						lien;
+	char					*groupe_u;
+	int						taille;
+	char					*date_heure;
+	struct s_compl_listing	*next;
+}							t_cliste;
+
+void						ft_erreur(char *str, int mode);
+int							*ft_detection_option(char *str, int *option);
+int							*ft_lecture_option(char ***argv);
+char						*ft_path(char *src, char *fichier);
 
 #endif
