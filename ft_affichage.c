35304acc29f1a72dca14b1ec7857a758685d65d5
Dylan BOURDON <dbourdon@student.42.fr>
2016-05-07 16:22:09 +0200

    save

--- ft_affichage.c
+++ ft_affichage.c
@@ -1,30 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_affichage.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/04/29 16:45:27 by dbourdon          #+#    #+#             */
-/*   Updated: 2016/05/07 16:21:53 by dbourdon         ###   ########.fr       */
+/*   Created: 2016/05/07 15:40:32 by dbourdon          #+#    #+#             */
+/*   Updated: 2016/05/07 16:21:51 by dbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int		main(int argc, char **argv)
+void	ft_affichage(t_liste lst_f, int *option, int argc)
 {
-	int		*option;
-	int		i;
-	t_liste	*lst_f;
-	char	**liste_arg;
+	if (option[0] != 1)
 
-	argv++;
-	i = 0;
-	option = ft_lecture_option(&(argv), &(argc));
-	liste_arg = ft_trie_tabtab(argv, argc);
-	while (liste_arg[i])
-		ft_lecture_liste(lst_f, argv[i++], option);
-	ft_affichage(lst_f, option, argc);
-	return (0);
 }
