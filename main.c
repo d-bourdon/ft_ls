90fa5de13b52f70fd9c629c751bd39ea1add187c
Dylan BOURDON <dbourdon@student.42.fr>
2016-05-19 17:52:41 +0200

    Save

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dbourdon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/29 16:45:27 by dbourdon          #+#    #+#             */
-/*   Updated: 2016/05/16 16:10:44 by dbourdon         ###   ########.fr       */
+/*   Updated: 2016/05/19 17:51:48 by dbourdon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,20 +21,12 @@ int		main(int argc, char **argv)
 
 	argv++;
 	i = 0;
-	printf("On start\n"); fflush(stdout);
 	option = ft_lecture_option(&(argv), &(argc));
-	printf("On as lu\n"); fflush(stdout);
 	liste_arg = ft_trie_tabtab(argv, argc);
 	if (argc == 1)
 		argc++;
-	printf("On as trier\n"); fflush(stdout);
 	while (i < (argc - 1) && liste_arg[i] != NULL)
-	{
-		printf("On boucle %s\n", liste_arg[i]); fflush(stdout);
 		ft_lecture_liste(&lst_f, liste_arg[i++], option);
-	}
-	printf("\n\nOn as fini de boucler\n"); fflush(stdout);
 	ft_affichage(lst_f, option, argc);
-	printf("Hehe on a fini !\n");fflush(stdout);
 	return (0);
 }
